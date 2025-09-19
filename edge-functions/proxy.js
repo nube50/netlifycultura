7461874f28d3fc84f202c5af0685a2b0c8285005
nube50 <dajomondra@gmail.com>
2025-09-19 10:48:53 -0500

    Update proxy.js

--- edge-functions/proxy.js
+++ edge-functions/proxy.js
@@ -6,7 +6,7 @@ export default async (req, context) => {
   }
 
   // URL de tu backend real con V2Ray
-  const backendUrl = "wss://thomas.culruravpn.site/culturavpn";
+  const backendUrl = "wss://thomas.culturavpn.site:444/";
 
   return context.rewrite(backendUrl);
 };
