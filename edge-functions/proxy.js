e35fd33ba6d7c6fed11aa43c415ce1c9f550806e
nube50 <dajomondra@gmail.com>
2025-09-19 11:25:41 -0500

    Update proxy.js

--- edge-functions/proxy.js
+++ edge-functions/proxy.js
@@ -1,12 +1,9 @@
 export default async (req, context) => {
   const path = new URL(req.url).pathname;
   const targetUrl = "https://thomas.culturavpn.site:444" + path;
-  
-  // Proxy transparente: reenvía todo el tráfico tal cual llega
   return context.rewrite(targetUrl);
 };
 
-// Aplica la función universalmente a cualquier ruta
 export const config = {
-  path: "/*"
+  path: "/master*"
 };
